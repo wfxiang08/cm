75e577978e7b5fbf9d4ad6ef5744279b802a7682
liuqi <liuqi@wandoujia.com>
2014-11-24 11:50:47 +0800

    add planbuilder && schema

---
+++ vt/tabletserver/planbuilder/dml.go
@@ -0,0 +1,149 @@
+// Copyright 2014, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package planbuilder
+
+import (
+	"strconv"
+
+	log "github.com/ngaut/logging"
+	"github.com/wandoulabs/cm/sqlparser"
+	"github.com/wandoulabs/cm/vt/schema"
+)
+
+func analyzeUpdate(upd *sqlparser.Update, getTable TableGetter) (plan *ExecPlan, err error) {
+	plan = &ExecPlan{
+		PlanId:    PLAN_PASS_DML,
+		FullQuery: GenerateFullQuery(upd),
+	}
+
+	tableName := sqlparser.GetTableName(upd.Table)
+	if tableName == "" {
+		plan.Reason = REASON_TABLE
+		return plan, nil
+	}
+	tableInfo, err := plan.setTableInfo(tableName, getTable)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(tableInfo.Indexes) == 0 || tableInfo.Indexes[0].Name != "PRIMARY" {
+		log.Warningf("no primary key for table %s", tableName)
+		plan.Reason = REASON_TABLE_NOINDEX
+		return plan, nil
+	}
+
+	plan.SecondaryPKValues, err = analyzeUpdateExpressions(upd.Exprs, tableInfo.Indexes[0])
+	if err != nil {
+		if err == TooComplex {
+			plan.Reason = REASON_PK_CHANGE
+			return plan, nil
+		}
+		return nil, err
+	}
+
+	plan.OuterQuery = GenerateUpdateOuterQuery(upd)
+
+	if conditions := analyzeWhere(upd.Where); conditions != nil {
+		pkValues, err := getPKValues(conditions, tableInfo.Indexes[0])
+		if err != nil {
+			return nil, err
+		}
+		if pkValues != nil {
+			plan.PlanId = PLAN_DML_PK
+			plan.PKValues = pkValues
+			return plan, nil
+		}
+	}
+
+	plan.PlanId = PLAN_DML_SUBQUERY
+	plan.Subquery = GenerateUpdateSubquery(upd, tableInfo)
+	return plan, nil
+}
+
+func analyzeDelete(del *sqlparser.Delete, getTable TableGetter) (plan *ExecPlan, err error) {
+	plan = &ExecPlan{
+		PlanId:    PLAN_PASS_DML,
+		FullQuery: GenerateFullQuery(del),
+	}
+
+	tableName := sqlparser.GetTableName(del.Table)
+	if tableName == "" {
+		plan.Reason = REASON_TABLE
+		return plan, nil
+	}
+	tableInfo, err := plan.setTableInfo(tableName, getTable)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(tableInfo.Indexes) == 0 || tableInfo.Indexes[0].Name != "PRIMARY" {
+		log.Warningf("no primary key for table %s", tableName)
+		plan.Reason = REASON_TABLE_NOINDEX
+		return plan, nil
+	}
+
+	plan.OuterQuery = GenerateDeleteOuterQuery(del)
+
+	if conditions := analyzeWhere(del.Where); conditions != nil {
+		pkValues, err := getPKValues(conditions, tableInfo.Indexes[0])
+		if err != nil {
+			return nil, err
+		}
+		if pkValues != nil {
+			plan.PlanId = PLAN_DML_PK
+			plan.PKValues = pkValues
+			return plan, nil
+		}
+	}
+
+	plan.PlanId = PLAN_DML_SUBQUERY
+	plan.Subquery = GenerateDeleteSubquery(del, tableInfo)
+	return plan, nil
+}
+
+func analyzeSet(set *sqlparser.Set) (plan *ExecPlan) {
+	plan = &ExecPlan{
+		PlanId:    PLAN_SET,
+		FullQuery: GenerateFullQuery(set),
+	}
+	if len(set.Exprs) > 1 { // Multiple set values
+		return plan
+	}
+	update_expression := set.Exprs[0]
+	plan.SetKey = string(update_expression.Name.Name)
+	numExpr, ok := update_expression.Expr.(sqlparser.NumVal)
+	if !ok {
+		return plan
+	}
+	val := string(numExpr)
+	if ival, err := strconv.ParseInt(val, 0, 64); err == nil {
+		plan.SetValue = ival
+	} else if fval, err := strconv.ParseFloat(val, 64); err == nil {
+		plan.SetValue = fval
+	}
+	return plan
+}
+
+func analyzeUpdateExpressions(exprs sqlparser.UpdateExprs, pkIndex *schema.Index) (pkValues []interface{}, err error) {
+	for _, expr := range exprs {
+		index := pkIndex.FindColumn(sqlparser.GetColName(expr.Name))
+		if index == -1 {
+			continue
+		}
+		if !sqlparser.IsValue(expr.Expr) {
+			log.Warningf("expression is too complex %v", expr)
+			return nil, TooComplex
+		}
+		if pkValues == nil {
+			pkValues = make([]interface{}, len(pkIndex.Columns))
+		}
+		var err error
+		pkValues[index], err = sqlparser.AsInterface(expr.Expr)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return pkValues, nil
+}
---
+++ vt/tabletserver/planbuilder/query_gen.go
@@ -0,0 +1,161 @@
+// Copyright 2014, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package planbuilder
+
+import (
+	"fmt"
+
+	"github.com/wandoulabs/cm/sqlparser"
+	"github.com/wandoulabs/cm/vt/schema"
+)
+
+func GenerateFullQuery(statement sqlparser.Statement) *sqlparser.ParsedQuery {
+	buf := sqlparser.NewTrackedBuffer(nil)
+	statement.Format(buf)
+	return buf.ParsedQuery()
+}
+
+func GenerateFieldQuery(statement sqlparser.Statement) *sqlparser.ParsedQuery {
+	buf := sqlparser.NewTrackedBuffer(FormatImpossible)
+	buf.Myprintf("%v", statement)
+	if buf.HasBindVars() {
+		return nil
+	}
+	return buf.ParsedQuery()
+}
+
+// FormatImpossible is a callback function used by TrackedBuffer
+// to generate a modified version of the query where all selects
+// have impossible where clauses. It overrides a few node types
+// and passes the rest down to the default FormatNode.
+func FormatImpossible(buf *sqlparser.TrackedBuffer, node sqlparser.SQLNode) {
+	switch node := node.(type) {
+	case *sqlparser.Select:
+		buf.Myprintf("select %v from %v where 1 != 1", node.SelectExprs, node.From)
+	case *sqlparser.JoinTableExpr:
+		if node.Join == sqlparser.AST_LEFT_JOIN || node.Join == sqlparser.AST_RIGHT_JOIN {
+			// ON clause is requried
+			buf.Myprintf("%v %s %v on 1 != 1", node.LeftExpr, node.Join, node.RightExpr)
+		} else {
+			buf.Myprintf("%v %s %v", node.LeftExpr, node.Join, node.RightExpr)
+		}
+	default:
+		node.Format(buf)
+	}
+}
+
+func GenerateSelectLimitQuery(selStmt sqlparser.SelectStatement) *sqlparser.ParsedQuery {
+	buf := sqlparser.NewTrackedBuffer(nil)
+	sel, ok := selStmt.(*sqlparser.Select)
+	if ok {
+		limit := sel.Limit
+		if limit == nil {
+			sel.Limit = execLimit
+			defer func() {
+				sel.Limit = nil
+			}()
+		}
+	}
+	buf.Myprintf("%v", selStmt)
+	return buf.ParsedQuery()
+}
+
+func GenerateSelectOuterQuery(sel *sqlparser.Select, tableInfo *schema.Table) *sqlparser.ParsedQuery {
+	buf := sqlparser.NewTrackedBuffer(nil)
+	fmt.Fprintf(buf, "select ")
+	writeColumnList(buf, tableInfo.Columns)
+	buf.Myprintf(" from %v where %a", sel.From, ":#pk")
+	return buf.ParsedQuery()
+}
+
+func GenerateInsertOuterQuery(ins *sqlparser.Insert) *sqlparser.ParsedQuery {
+	buf := sqlparser.NewTrackedBuffer(nil)
+	buf.Myprintf("insert %vinto %v%v values %a%v",
+		ins.Comments,
+		ins.Table,
+		ins.Columns,
+		":#values",
+		ins.OnDup,
+	)
+	return buf.ParsedQuery()
+}
+
+func GenerateUpdateOuterQuery(upd *sqlparser.Update) *sqlparser.ParsedQuery {
+	buf := sqlparser.NewTrackedBuffer(nil)
+	buf.Myprintf("update %v%v set %v where %a", upd.Comments, upd.Table, upd.Exprs, ":#pk")
+	return buf.ParsedQuery()
+}
+
+func GenerateDeleteOuterQuery(del *sqlparser.Delete) *sqlparser.ParsedQuery {
+	buf := sqlparser.NewTrackedBuffer(nil)
+	buf.Myprintf("delete %vfrom %v where %a", del.Comments, del.Table, ":#pk")
+	return buf.ParsedQuery()
+}
+
+func GenerateSelectSubquery(sel *sqlparser.Select, tableInfo *schema.Table, index string) *sqlparser.ParsedQuery {
+	hint := &sqlparser.IndexHints{Type: sqlparser.AST_USE, Indexes: [][]byte{[]byte(index)}}
+	table_expr := sel.From[0].(*sqlparser.AliasedTableExpr)
+	savedHint := table_expr.Hints
+	table_expr.Hints = hint
+	defer func() {
+		table_expr.Hints = savedHint
+	}()
+	return GenerateSubquery(
+		tableInfo.Indexes[0].Columns,
+		table_expr,
+		sel.Where,
+		sel.OrderBy,
+		sel.Limit,
+		false,
+	)
+}
+
+func GenerateUpdateSubquery(upd *sqlparser.Update, tableInfo *schema.Table) *sqlparser.ParsedQuery {
+	return GenerateSubquery(
+		tableInfo.Indexes[0].Columns,
+		&sqlparser.AliasedTableExpr{Expr: upd.Table},
+		upd.Where,
+		upd.OrderBy,
+		upd.Limit,
+		true,
+	)
+}
+
+func GenerateDeleteSubquery(del *sqlparser.Delete, tableInfo *schema.Table) *sqlparser.ParsedQuery {
+	return GenerateSubquery(
+		tableInfo.Indexes[0].Columns,
+		&sqlparser.AliasedTableExpr{Expr: del.Table},
+		del.Where,
+		del.OrderBy,
+		del.Limit,
+		true,
+	)
+}
+
+func GenerateSubquery(columns []string, table *sqlparser.AliasedTableExpr, where *sqlparser.Where, order sqlparser.OrderBy, limit *sqlparser.Limit, for_update bool) *sqlparser.ParsedQuery {
+	buf := sqlparser.NewTrackedBuffer(nil)
+	if limit == nil {
+		limit = execLimit
+	}
+	fmt.Fprintf(buf, "select ")
+	i := 0
+	for i = 0; i < len(columns)-1; i++ {
+		fmt.Fprintf(buf, "%s, ", columns[i])
+	}
+	fmt.Fprintf(buf, "%s", columns[i])
+	buf.Myprintf(" from %v%v%v%v", table, where, order, limit)
+	if for_update {
+		buf.Myprintf(sqlparser.AST_FOR_UPDATE)
+	}
+	return buf.ParsedQuery()
+}
+
+func writeColumnList(buf *sqlparser.TrackedBuffer, columns []schema.TableColumn) {
+	i := 0
+	for i = 0; i < len(columns)-1; i++ {
+		fmt.Fprintf(buf, "%s, ", columns[i].Name)
+	}
+	fmt.Fprintf(buf, "%s", columns[i].Name)
+}
