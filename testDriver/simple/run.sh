ebcb4aba49dafff86cea18fa201c6be675d999d1
liuqi <liuqi@wandoujia.com>
2014-12-18 13:52:17 +0800

    fix test script

--- testDriver/simple/run.sh
+++ testDriver/simple/run.sh
@@ -2,7 +2,9 @@
 
 mv ../../etc/cfg.json ../../etc/cfg.json.bak
 cp cfg.json ../../etc/cfg.json
-curl http://localhost:8888/api/reload
 
 go run bench.go -t init -p 3306
+
+
+curl http://localhost:8888/api/reload
 go run bench.go -t type-test
