0713168cabff843c0f92f9a8df73c553a7745a35
liuqi <liuqi@wandoujia.com>
2015-01-09 17:37:23 +0800

    add missing file

---
+++ proxy/util.go
@@ -0,0 +1,28 @@
+package proxy
+
+import (
+	"sync"
+
+	. "github.com/wandoulabs/cm/mysql"
+	"github.com/wandoulabs/cm/vt/schema"
+)
+
+type execTask struct {
+	wg   *sync.WaitGroup
+	rs   []interface{}
+	idx  int
+	co   *SqlConn
+	sql  string
+	args []interface{}
+}
+
+func GetRowCacheType(rowCacheType string) int {
+	switch rowCacheType {
+	case "RW":
+		return schema.CACHE_RW
+	case "W":
+		return schema.CACHE_W
+	default:
+		return schema.CACHE_NONE
+	}
+}
