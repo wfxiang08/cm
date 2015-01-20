10bceab0404da7545f2067e46c8c903a121f765f
dongxu <huang@wandoujia.com>
2015-01-20 19:30:18 +0800

    update test script

--- java_driver_test/run_test.sh
+++ java_driver_test/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd cm-test
-mysql -e "DROP DATABASE IF EXISTS test; CREATE DATABASE test;CREATE TABLE test.tbl_test (id INT, data VARCHAR(255));" -uroot
+mysql -e "DROP DATABASE IF EXISTS test; CREATE DATABASE test;CREATE TABLE test.tbl_test (id INT, data VARCHAR(255));" -uroot -P 3306 --host 127.0.0.1
 mvn install && mvn assembly:assembly && java -jar target/com.wandoujia.cm-test-1.0-SNAPSHOT.jar
