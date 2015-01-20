f3c4ea78c4b25d5244354130b13c1aabf3fa75bb
dongxu <huang@wandoujia.com>
2015-01-20 19:28:21 +0800

    add create table in shell

--- java_driver_test/run_test.sh
+++ java_driver_test/run_test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd cm-test
+mysql -e "DROP DATABASE IF EXISTS test; CREATE DATABASE test;CREATE TABLE test.tbl_test (id INT, data VARCHAR(255));" -uroot
 mvn install && mvn assembly:assembly && java -jar target/com.wandoujia.cm-test-1.0-SNAPSHOT.jar
